1a8e04ec5d50687d3fbea7b6af397914e8798b96
sundarcodes <sundar82@gmail.com>
2017-09-04 11:16:15 +0530

    Introduced Routing and added archive link

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -1,23 +1,28 @@
+import { routes } from './app.routes';
 import { TodoService } from './services/todo.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeComponent } from './home/home.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { ArchiveComponent } from './archive/archive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     HomeComponent,
-    TodoListComponent
+    TodoListComponent,
+    ArchiveComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ TodoService ],
   bootstrap: [AppComponent]
